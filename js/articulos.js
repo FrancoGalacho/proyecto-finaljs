5a87de0aae3c779b3d7db801b8c428b6798e98b3
franco galacho <franco.galacho@hotmail.com>
2023-01-25 13:02:31 -0300

    seguimos con javascript

--- js/articulos.js
+++ js/articulos.js
@@ -1,13 +1,16 @@
 const search = document.querySelector("#searchbox"),
-    buscar = document.querySelector("#form_buscar")
+buscar = document.querySelector("#form_buscar")
 contenedor = document.querySelector("#productos"),
-    nombreUser = document.querySelector("#nombreUser")
+nombreUser = document.querySelector("#nombreUser")
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
 
-    
+
+let carrito = [];
 
 fetch("../data/data.json")
-.then(res=>res.json())
-.then(datos=>{
+.then(res => res.json())
+.then(datos => {
     crearHTML(datos);
 })
 
@@ -23,32 +26,85 @@ buscar.addEventListener("submit", (e) => {
 
 
 // function filtrarproductos(filtro) {
-//     let filtrado = productos.filter((el) => {
-//         return el.nombre.includes(filtro);
-
-//     });
-//     return filtrado;
-// }
-
-function crearHTML (array){
-    contenedor.innerHTML = "";
-    for (const producto of array) {
-        contenedor.innerHTML += `
-        <div class="card " style="width: 18rem;">
-  <img src="${producto.img}" class="card-img-top tarjeta" alt="...">
-  <div class="card-body">
-    <h2 class="card-title">${producto.nombre}</h2>
-    <h5 class="md-card-subtitle mbsc-align-center mbsc-bold">$${producto.precio}</h5>
-    <a class="btn btn-primary btn-comprar"   >Comprar</a>
-  </div>
-</div>`
+    //     let filtrado = productos.filter((el) => {
+        //         return el.nombre.includes(filtro);
         
-    }
-}
+        //     });
+        //     return filtrado;
+        // }
+        
+        
+        function crearHTML(array) {
+            contenedor.innerHTML = "";
+            for (const producto of array) {
+                let card = document.createElement("div");
+                card.innerHTML = `
+                <div class="md-card-bg mbsc-card">
+                <img src="${producto.img}"/>
+                <h2 class="md-card-title mbsc-align-center">${producto.nombre}</h2>
+                <h5 class="md-card-subtitle mbsc-align-center mbsc-bold">$${producto.precio}</h5>
+                </div>
+                `
+                contenedor.append(card);
+                let comprar = document.createElement('button');
+                comprar.innerText ="comprar";
+                comprar.className= "comprar";
+                
+                contenedor.append(comprar);
+            
+                comprar.addEventListener(`click`, ()=>{
+                    carrito.push({
+                        id: producto.id,
+                        nombre: producto.nombre,
+                        precio: producto.precio,
+                    });
+                    
+                carrito.forEach((producto)=>{
+                    let carritoContent = document.createElement("div");
+                    carritoContent.className= "modalCarrito";
+                    carritoContent.innerHTML= ` <img src="${producto.img}"/>
+                    <h3 >${producto.nombre}</h3>
+                    <p>$${producto.precio}$</p>
+                    `;
+                
+                modalContainer.append(carritoContent);
+            
+                        
+           
+            
+        });
+});
+
+verCarrito.addEventListener("click",()=>{
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+    modalHeader.innerHTML = `
+    <h1 class="modal-header-title">Carrito.</h1>
+    `;
+    modalContainer.append(modalHeader);
+
+    const modalboton = document.createElement("h1");
+    modalboton.innerText = "tus productos";
+    modalboton.className = "modal-header-button";
+
+    modalHeader.append(modalboton);
+    
+    const totalPrecio = document.createElement("div");
+    totalPrecio.className = "modal-content";
+    totalPrecio.innerHTML = `total a pagar: ${total} $`;
+    modalContainer.append(totalPrecio);
+;
+});
 
 
-const input = document.querySelectorAll('btn-comprar');
-console.log (input);
+const total = carrito.reduce ((acc,el) => acc + el.precio, 0);
+
+const totalPrecio = document.createElement("div");
+totalPrecio.className = "modal-content";
+totalPrecio.innerHTML = `total a pagar: ${total} $`;
+modalContainer.append(totalPrecio);
+};
+        }
 
 
 
